eeb4370e7f20bcb6346f0cd58cf225743a0a98ef
Jarryd-pretorius <86589746+Jarryd-pretorius@users.noreply.github.com>
2021-07-16 16:21:16 +0800

    finnished lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+var selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier =  function(multi) {
+ return function(money) {
+    return multi * money
+
+
+ };
+};
+
+var multipliedVar = createFareMultiplier();
+
+
+const fareDoubler = function(multipliedVar) {
+    return multipliedVar * 2 
+}
+
+const fareTripler = function(multipliedVar) {
+    return multipliedVar * 3
+}
+
+const selectDifferentDrivers = function (drivers, driversToReturn) {
+    return driversToReturn(drivers);
+  };
